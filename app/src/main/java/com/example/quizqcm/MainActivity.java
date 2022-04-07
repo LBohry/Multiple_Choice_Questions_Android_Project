bcf82266f6b552ea6495755b163fefb8c0062bdd
LBohry <83679014+LBohry@users.noreply.github.com>
2022-04-07 15:58:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/quizqcm/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.quizqcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_suiv;
+    RadioGroup rg;
+    RadioButton rb;
+    int Score =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_suiv = (Button) findViewById(R.id.btn_suiv);
+        rg = (RadioGroup) findViewById(R.id.radio);
+        btn_suiv.setOnClickListener(this);
+        }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId())
+        {
+                case R.id.btn_suiv:
+                    int id = rg.getCheckedRadioButtonId();
+                    rb =(RadioButton) findViewById(id);
+                    if (rb.getText().toString().equals("7") )
+                {
+                    Score = Score + 1;
+                }
+                    Intent i = new Intent(this,Question2.class);
+                    i.putExtra("score",Score);
+                    startActivity(i);
+
+
+
+        }
+    }
+}
